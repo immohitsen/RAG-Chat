0e4f8e1ef6ab34e5c4529cd196d8aab0aa183b4a
immohitsen <senmohit9005@gmail.com>
2026-03-26 00:44:35 +0530

    new

--- src/search.py
+++ src/search.py
@@ -17,7 +17,7 @@ class RAGSearch:
             self.vectorstore.build_from_documents(docs)
         else:
             self.vectorstore.load()
-        groq_api_key = ""
+        groq_api_key = os.getenv("GROQ_API_KEY")
         self.llm = ChatGroq(groq_api_key=groq_api_key, model_name=llm_model)
         print(f"[INFO] Groq LLM initialized: {llm_model}")
 
